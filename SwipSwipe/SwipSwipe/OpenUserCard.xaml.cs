9dc2c1bd9b511185da4457b007aba77bfe3b0e1b
Alex Ritt <AlexRitt06@outlook.com>
2021-04-25 12:51:39 -0500

    Post Outlines

--- SwipSwipe/SwipSwipe/OpenUserCard.xaml.cs
+++ SwipSwipe/SwipSwipe/OpenUserCard.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SwipSwipe;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +14,22 @@ namespace SwipSwipe
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class OpenUserCard : ContentView
     {
+        public static readonly BindableProperty UserProperty = BindableProperty.Create("User", typeof(User), typeof(OpenUserCard), null);
+
+        public User UserCard
+        {
+            get => (User)GetValue(UserProperty);
+            set => SetValue(UserProperty, value);
+        }
+
         public OpenUserCard()
         {
             InitializeComponent();
         }
+
+        //public string ImageSource => UserCard.ProfilePicture.AbsoluteUri;
+        public string Image = "Resources/drawable/SearchIcon.png";
+
+        public ImageSource imageSource { get { return ImageSource.FromFile(Image); } }
     }
 }
---
+++ SwipSwipe/SwipSwipe/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwipSwipe
+{
+    public class User
+    {
+        public string UserID { get; }
+
+        public string ProfileID { get; }
+
+        public Uri ProfilePicture { get; set; }
+
+        public string UserTag { get; set; }
+
+        public string UserName { get; set; }
+
+        public Post[] Posts { get; set; }
+
+        public int Score { get; set; }
+
+        public int Followers { get; set; }
+
+        public int Following { get; set; }
+    }
+
+    public class Post
+    {
+        public User Poster { get; }
+
+        public string PostID { get; }
+
+        public Uri PostLink { get; }
+
+        public string Caption { get; set; }
+
+        public int ViewCount { get; set; }
+
+        public User[] LikeCount { get; set; }
+
+        public Comment[] Comments { get; set; }
+    }
+
+    public class Comment
+    {
+        public User Poster { get; }
+
+        public string Content { get; set; }
+
+        public User[] LikeCount { get; set; }
+    }
+}
